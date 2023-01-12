72fdbbe0ce0f46426dc40955b34bc1e1cd012821
Rick Mays <8709540+rickmays@users.noreply.github.com>
2023-01-12 16:32:36 -0600

    add getStaticProps, getStaticPaths - thru section 106

---
+++ pages/[pid].js
@@ -0,0 +1,58 @@
+import { Fragment } from "react";
+import path from "path";
+import fs from "fs/promises";
+
+function ProductDetailPage(props) {
+  const { loadedProduct } = props;
+
+  // Not needed if fallback below set to 'blocking'
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <Fragment>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </Fragment>
+  );
+}
+
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
+export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map(product => product.id);
+  const pathsWithParams = ids.map(id => ({ params: { pid: id } }));
+
+  return {
+    paths: pathsWithParams,
+    fallback: true, // true | false | 'blocking'
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+
+  const data = await getData();
+
+  const product = data.products.find(product => product.id === productId);
+
+  if (!product) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export default ProductDetailPage;
