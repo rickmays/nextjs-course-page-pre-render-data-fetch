ef7bcd3182c482d7d9cbbab3317305e131d683af
Rick Mays <8709540+rickmays@users.noreply.github.com>
2023-01-13 17:13:30 -0600

    add client-side fetching, including useSWR - thru section 114

---
+++ pages/last-sales.js
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+function LastSalesPage() {
+  const [sales, setSales] = useState();
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // using SWR instead of useEffect
+  const fetcher = url => fetch(url).then(res => res.json());
+  const { data, error } = useSWR(
+    "https://nextjs-course-caeb6-default-rtdb.firebaseio.com/sales.json",
+    fetcher
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  if (error) {
+    return <p>Failed to load.</p>;
+  }
+
+  if (!data || !sales) {
+    return <p>Loading...</p>;
+  }
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch("https://nextjs-course-caeb6-default-rtdb.firebaseio.com/sales.json")
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       const transformedSales = [];
+
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  // if (isLoading) {
+  //   return <p>Loading...</p>;
+  // }
+
+  // if (!sales) {
+  //   return <p>No data yet</p>;
+  // }
+
+  return (
+    <ul>
+      {sales.map(sale => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default LastSalesPage;
